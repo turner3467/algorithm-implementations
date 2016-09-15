fff2924a0369ffe668a22baaf0d30c43fb41a510
Jeremy Turner <jturner@retailinsight.co.uk>
2016-09-15 11:29:50 +0100

    Change to bayesian approach

--- metropolis-hastings.R
+++ metropolis-hastings.R
@@ -22,29 +22,6 @@ beta.5.prior <- rnorm(n = sample.size)
 prior <- as.matrix(cbind(beta.0.prior, beta.1.prior, beta.2.prior,
                          beta.3.prior, beta.4.prior, beta.5.prior))
 
-myProb <- function(b, x){
-    tmp <- b[7] - b[1]*x[1] - b[2]*x[2] - b[3]*x[3] - b[4]*x[4] - b[5]*x[5] - b[6]*x[6]
-    tmp <- exp(-tmp / (2*pi*sigma.2)) / sqrt(2*pi*sigma.2)
-    return(tmp)
-}
-
-myFunc <- function(a, data){
-    tmp <- apply(data, MARGIN = 1, FUN = myProb, a)
-    return(prod(tmp))
-}
-
-myFunc1 <- function(a, data){
-    result = 1
-    for(i in 1:dim(data)[1]){
-        result <- result * myProb(data[i,], a)
-    }
-    return(result)
-}
-
-evidence <- apply(prior, MARGIN = 1, FUN = myFunc1, as.matrix(df))
-
-posterior <- evidence * prior
-
 myPlot <- function(df){
     tmp <- as.data.frame(df[, -c(1)])
     tmp <- melt(tmp)
