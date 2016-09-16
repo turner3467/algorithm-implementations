11f64ec84cacdb51916097acd349e137d13cd8db
Jeremy Turner <jturner@retailinsight.co.uk>
2016-09-16 17:35:53 +0100

    Starting neural net toy problem

---
+++ logic_gate_neural_net.R
@@ -0,0 +1,5 @@
+## Toy project creating simple neural networks to create logic gates
+
+and.gate <- data.frame(x1 = c(0, 0, 1, 1), x2 = c(0, 1, 0, 1), y = c(0, 0, 0, 1))
+or.gate <- data.frame(x1 = c(0, 0, 1, 1), x2 = c(0, 1, 0, 1), y = c(0, 1, 1, 1))
+xor.gate <- data.frame(x1 = c(0, 0, 1, 1), x2 = c(0, 1, 0, 1), y = c(0, 1, 1, 0))
